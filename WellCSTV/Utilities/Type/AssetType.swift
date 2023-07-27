18a5da28c4f3c116e97eb4932d54e39667b23605
Wellington Nascente Hirsch <wnhirsch@inf.ufrgs.br>
2023-07-27 20:38:02 -0300

    close #4

---
+++ WellCSTV/Utilities/Type/AssetType.swift
@@ -0,0 +1,12 @@
+//
+//  AssetType.swift
+//  WellCSTV
+//
+//  Created by Wellington Nascente Hirsch on 27/07/23.
+//
+
+import Foundation
+
+enum AssetType: String, CaseIterable {
+    case logo = "fuze-logo"
+}
---
+++ WellCSTV/Utilities/Type/ColorType.swift
@@ -0,0 +1,15 @@
+//
+//  ColorType.swift
+//  WellCSTV
+//
+//  Created by Wellington Nascente Hirsch on 27/07/23.
+//
+
+import SwiftUI
+ 
+extension Color {
+    
+    // MARK: - CSTV Colors
+    /// background = #161621
+    static let cstvBackground = Color(hex: "#161621")
+}
