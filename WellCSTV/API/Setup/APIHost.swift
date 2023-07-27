ecb4150f317fd120a7e0cb8c3f9cdc242c96d2e0
Wellington Nascente Hirsch <wnhirsch@inf.ufrgs.br>
2023-07-27 19:42:26 -0300

    close #1

---
+++ WellCSTV/API/Setup/APIHost.swift
@@ -0,0 +1,20 @@
+//
+//  APIHost.swift
+//  WellCSTV
+//
+//  Created by Wellington Nascente Hirsch on 27/07/23.
+//
+
+import Foundation
+
+enum APIHost {
+    
+    static var baseURL: URL {
+        URL(string: "https://api.pandascore.co/csgo")!
+    }
+    
+    // It's not safe to put it hard coded, but for test purposes it's easier being right here
+    static var accessToken: String = "Cm61BGQzysAEp-8LfsNGuISCKscvZXBieDoisJ2UCdDLQ3cDpd0"
+    
+    static var itemsPerPage: Int = 10
+}
---
+++ WellCSTV/Application/WellCSTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  WellCSTVApp.swift
+//  WellCSTV
+//
+//  Created by Wellington Nascente Hirsch on 27/07/23.
+//
+
+import SwiftUI
+
+@main
+struct WellCSTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
